2f65347d088034595b405ad323978e0f26f8902a
vbarsott <vaplacencio@hotmail.com>
2021-07-23 17:09:16 -0400

    Complete portfolio

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ The following technologies were used to develop this website:
 
 ## 💻 What was installed?
 
-# GitHub setup
+### GitHub setup
 
     echo "# react-image-gallery" >> README.md
     git init
@@ -23,13 +23,16 @@ The following technologies were used to develop this website:
     git push -u origin main
     disable node_modules on gitignore (# dependencies node_modules)
 
-# React setup
+### React setup
 
     npx create-react-app react-image-gallery
     cd react-image-gallery/
     npm start
     npm i react-router-dom
+    npm i postcss-cli
+    npm i autoprefixer
 
-# TailwindCSS setup
+### TailwindCSS setup
 
-    npm i -D tailwind
+    npm i -D tailwindcss
+    npx tailwind init tailwind.js
--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import ImageCard from './components/ImageCard';
+import ImageSearch from './components/ImageSearch';
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, [term]);
+
   return (
-    <header>
-      <h1>App Component</h1>
-    </header>
+    <div className='container mx-auto'>
+      <ImageSearch searchText={(text) => setTerm(text)} />
+      {!isLoading && images.length === 0 && (
+        <h1 className='text-5xl text-center mx-auto mt-32'>No Images</h1>
+      )}
+      {isLoading ? (
+        <h1 className='text-6xl text-center mx-auto mt-32'>Loading...</h1>
+      ) : (
+        <div className='grid grid-cols-3 gap-4'>
+          {images.map((image) => (
+            <ImageCard key={image.id} image={image} />
+          ))}
+        </div>
+      )}
+    </div>
   );
 }
 
